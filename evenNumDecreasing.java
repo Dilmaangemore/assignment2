4fe30c5f518d65f66152c9a6dbf1eb7b374fc6fb
Dilmaangemore <90092966+Dilmaangemore@users.noreply.github.com>
2022-01-02 12:26:42 +0530

    recursion

---
+++ evenNumDecreasing.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class evenNumDecreasing {
+	 static  void print (int i){
+		 
+		   //print even number in  decreasing order 
+		       int n=76;
+		           if (i>n) {
+		            return;
+		        }
+		           print(i+1);
+		          
+		        if(i%2==0)
+		        System.out.println("even no"+i);
+		     
+		        
+		         
+		    } 
+	 
+	 
+		public static void main(String[] args) {
+			int n=1;
+			print  (n);//print  (1);  
+		}
+}
